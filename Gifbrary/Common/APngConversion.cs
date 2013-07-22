dd92b01705725e9e53907e6f7fceed396072fa4c
maximusvladimir <pyram3d@gmail.com>
2013-07-22 16:42:21 -0500

    Seperated APNG converter.

--- Gifbrary/Common/APngConversion.cs
+++ Gifbrary/Common/APngConversion.cs
@@ -14,29 +14,23 @@ using SharpApng;
 
 namespace Gifbrary.Common
 {
-    public class GifConversion : VideoFramePullerConversion
+    public class APngConversion : VideoFramePullerConversion
     {
-        public GifConversion(Exportable ext, int loop)
-            :base(ext)
+        public APngConversion(Exportable ext, int loop)
+            : base(ext)
         {
             Loop = loop;
         }
 
         public int Loop
         {
-            get;set;
+            get;
+            set;
         }
 
         public override void Convert()
         {
-            //SharpApng.Apng ping = new Apng();
-            AnimatedGifEncoder e = new AnimatedGifEncoder();
-            e.Start(ExportData.DestinationFilePath);
-            e.SetQuality(21 - ((ExportData.Quality * 20) / 100));
-            e.SetDelay(1000 / ExportData.FPS);
-            e.SetRepeat(Loop);
-            if (ExportData.ChromaKey != null)
-                e.SetTransparent((Color)ExportData.ChromaKey);
+            Apng ping = new Apng();
             //e.SetSize(Width, Height);
             SetupEncoder();
             if (kill)
@@ -46,12 +40,10 @@ namespace Gifbrary.Common
                 if (kill)
                     return;
                 Image b = GetFrame(i);
-                e.AddFrame(b);
-              //  ping.AddFrame((Bitmap)b, 10, 10);
+                ping.AddFrame((Bitmap)b, 10, 10);
                 OnProgressChanged(i);
             }
-            //ping.WriteApng(ExportData.DestinationFilePath.Replace(".gif",".apng"),true,true);
-            e.Finish();
+            ping.WriteApng(ExportData.DestinationFilePath.Replace(".gif",".apng"),true,true);
             IsDone = true;
         }
     }
