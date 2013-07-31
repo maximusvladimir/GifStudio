69b0a2efa5f78a3dfff9095d3cdea6656106deaf
Max K. <pyram3d@gmail.com>
2013-07-31 15:52:11 -0500

    Added new converter.

---
+++ Gifbrary/Common/VideoFramePullerConversion.cs
@@ -0,0 +1,33 @@
+﻿using Interop.qedit;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Gifbrary.Common
+{
+    public class VideoFramePullerConversion : Conversion
+    {
+        public VideoFramePullerConversion(Exportable export)
+            : base(export)
+        { }
+
+        public override void SetupEncoder()
+        {
+
+        }
+
+        public override int GetTotalFrames()
+        {
+            return 0;
+        }
+
+        public override System.Drawing.Image GetFrame(int i)
+        {
+            return null;
+        }
+    }
+}
